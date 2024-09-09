bfcb93bc4c31783f967bc6646d2224ee7ea000e5
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2024-09-09 18:43:11 -0300

    sun

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = ({ onProductosClick, productos }) => {
                 </g>
             </svg>
             ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" width=".18in" height=".18in" viewBox="2 0 20 20">
+            <svg xmlns="http://www.w3.org/2000/svg" width=".18in" height=".18in" viewBox="5 2 20 20">
                 <g id="sun" transform="translate(3, 0)">
                 <path d="M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z" fill="none" stroke="#000000" strokeWidth="1.08" strokeLinecap="round" strokeLinejoin="round"
                 />
