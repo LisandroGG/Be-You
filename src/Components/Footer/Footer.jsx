3dc7e230c76cce68a689f01efa8e2465ae7146cd
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2024-09-09 13:08:10 -0300

    footer finish

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
     return(
-        <footer className="bg-[#00628B]">
-            <h1>Footer</h1>
+        <footer className="bg-[#00628B] flex flex-col items-center p-2 text-white font-medium text-lg md:text-xl">
+            <p>© 2024 | Be You</p>
+            <p><a href="https://lisandrodev.vercel.app/" target='_blank'>Diseño web Lisandro Pereyra</a></p>
         </footer>
     )
 }
